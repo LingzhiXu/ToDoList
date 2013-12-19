4cb51183e46f81157dabfc137a6f24b28a388494
Lingzhi Xu <lingzhixu@Lingzhis-MacBook-Air.local>
2013-12-18 22:44:21 -0500

    Initial Commit

---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Lingzhi Xu on 12/18/13.
+//  Copyright (c) 2013 Lingzhi Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
